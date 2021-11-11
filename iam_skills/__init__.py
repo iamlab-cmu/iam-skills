d5bc415fe864c587b218a30b10a5946de1624242
Kevin Zhang <kevinleezhang@gmail.com>
2021-11-11 18:04:45 -0500

    Added one step quat pose dmp skill.

--- iam_skills/__init__.py
+++ iam_skills/__init__.py
@@ -5,3 +5,4 @@ from .force_skill import *
 from .one_step_joint_skill import *
 from .one_step_pose_skill import *
 from .one_step_joint_dmp_skill import *
+from .one_step_quat_pose_dmp_skill import *
